3cb7ce3b10cc1c995fcb571d5a07b8636824ef66
Dave Jones <dave@waveform.org.uk>
2021-12-08 16:53:35 +0000

    Restructure the database to split out options

--- gather.py
+++ gather.py
@@ -14,69 +14,66 @@ from time import sleep
 create_sql = """
 PRAGMA foreign_keys = 1;
 
-CREATE TABLE compressors (
-    compressor TEXT NOT NULL,
-
-    CONSTRAINT compressors_pk PRIMARY KEY (compressor)
-);
-
-CREATE TABLE options (
+CREATE TABLE tests (
     compressor TEXT NOT NULL,
     options    TEXT NOT NULL,
+    level      TEXT NOT NULL,
 
-    CONSTRAINT options_pk PRIMARY KEY (compressor, options),
-    CONSTRAINT options_compressor_fk FOREIGN KEY (compressor)
-        REFERENCES compressors (compressor) ON DELETE CASCADE
+    CONSTRAINT options_pk PRIMARY KEY (compressor, options, level)
 );
+
+CREATE VIEW compressors AS
+    SELECT DISTINCT compressor FROM tests;
+
+CREATE VIEW options AS
+    SELECT DISTINCT compressor, options FROM tests;
 
 CREATE TABLE results (
     machine      TEXT         NOT NULL,
     arch         TEXT         NOT NULL,
     compressor   TEXT         NOT NULL,
     options      TEXT         NOT NULL,
+    level        TEXT         NOT NULL,
     succeeded    INTEGER      NOT NULL DEFAULT 0,
     elapsed      NUMERIC(8,2) NOT NULL,
     max_resident INTEGER      NOT NULL,
     ratio        NUMERIC(8,7) NOT NULL,
 
-    CONSTRAINT compression_pk PRIMARY KEY (machine, arch, compressor, options),
-    CONSTRAINT compression_options_fk FOREIGN KEY (compressor, options)
-        REFERENCES options (compressor, options) ON DELETE CASCADE,
+    CONSTRAINT compression_pk
+        PRIMARY KEY (machine, arch, compressor, options, level),
+    CONSTRAINT compression_options_fk
+        FOREIGN KEY (compressor, options, level)
+        REFERENCES tests (compressor, options, level) ON DELETE CASCADE,
     CONSTRAINT compression_succeeded_ck CHECK (succeeded IN (0, 1)),
     CONSTRAINT compression_elapsed_ck CHECK (elapsed >= 0.0),
     CONSTRAINT compression_resident_ck CHECK (max_resident >= 0),
     CONSTRAINT compression_ratio_ck CHECK (ratio >= 0)
 );
 
-CREATE INDEX results_options ON results(compressor, options);
+CREATE INDEX results_options ON results(compressor, options, level);
 """
 
 populate_sql = """
-INSERT OR IGNORE INTO compressors VALUES
-    ('zstd'),
-    ('gzip'),
-    ('lz4'),
-    ('xz');
-
-WITH RECURSIVE t(level, options) AS (
+WITH RECURSIVE
+c(compressor, options, min_level, max_level) AS (
+    VALUES
+        ('zstd', '',    1, 19),
+        ('zstd', '-T0', 1, 19),
+        ('gzip', '',    1, 9),
+        ('lz4',  '',    1, 9),
+        ('xz',   '',    0, 9),
+        ('xz',   '-e',  0, 9)
+),
+t(level, option) AS (
     VALUES (0, '-0')
     UNION ALL
     SELECT level + 1, '-' || CAST(level + 1 AS TEXT)
     FROM t
     WHERE level <= 20
 )
-INSERT OR IGNORE INTO options
-    SELECT 'zstd', options FROM t WHERE level BETWEEN 1 AND 19
-    UNION ALL
-    SELECT 'zstd', '-T0 ' || options FROM t WHERE level BETWEEN 1 AND 19
-    UNION ALL
-    SELECT 'gzip', options FROM t WHERE level BETWEEN 1 AND 9
-    UNION ALL
-    SELECT 'lz4', options FROM t WHERE level BETWEEN 1 AND 9
-    UNION ALL
-    SELECT 'xz', options FROM t WHERE level BETWEEN 0 AND 9
-    UNION ALL
-    SELECT 'xz', '-e ' || options FROM t WHERE level BETWEEN 0 AND 9;
+INSERT OR IGNORE INTO tests
+    SELECT c.compressor, c.options, t.option
+    FROM c JOIN t ON t.level BETWEEN c.min_level AND c.max_level
 """
 
 query_sql = """
@@ -84,15 +81,15 @@ WITH all_machines(machine, arch) AS (
     VALUES (?, ?)
 ),
 all_runs AS (
-    SELECT machine, arch, compressor, options
-    FROM all_machines CROSS JOIN options
+    SELECT machine, arch, compressor, options, level
+    FROM all_machines CROSS JOIN tests
 )
-SELECT machine, arch, compressor, options FROM all_runs
+SELECT machine, arch, compressor, options, level FROM all_runs
 EXCEPT
-SELECT machine, arch, compressor, options FROM results
+SELECT machine, arch, compressor, options, level FROM results
 """
 
-insert_sql = "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+insert_sql = "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 
 def get_db(filename):
@@ -110,11 +107,13 @@ def get_db(filename):
     return conn
 
 
-def run_test(compressor, options, filename):
+def run_test(compressor, options, level, filename):
     with io.open(filename, 'rb') as data:
         input_size = data.seek(0, io.SEEK_END)
         data.seek(0)
-        cmdline = ['time', '-f', '%E %M', compressor] + shlex.split(options)
+        cmdline = ['time', '-f', '%E %M', compressor, level]
+        cmdline += shlex.split(options)
+        print(shlex.join(cmdline), file=sys.stderr)
         proc = sp.Popen(
             cmdline, bufsize=0,
             stdin=data, stdout=sp.PIPE, stderr=sp.PIPE)
@@ -171,18 +170,19 @@ def main(args=None):
             return 1
 
     for row in db.execute(query_sql, (config.machine, config.arch)):
-        print(f'Testing {row["compressor"]} {row["options"]}', file=sys.stderr)
         try:
             elapsed, ratio, max_resident = run_test(
-                row['compressor'], row['options'], config.data)
+                row['compressor'], row['options'], row['level'], config.data)
         except RuntimeError:
             results = (
-                config.machine, config.arch, row['compressor'], row['options'],
-                0, 0.0, 0, 0)
+                config.machine, config.arch,
+                row['compressor'], row['options'], row['level'],
+                False, 0.0, 0, 0)
         else:
             results = (
-                config.machine, config.arch, row['compressor'], row['options'],
-                1, elapsed, max_resident, ratio)
+                config.machine, config.arch,
+                row['compressor'], row['options'], row['level'],
+                True, elapsed, max_resident, ratio)
         with db:
             db.execute(insert_sql, results)
 
